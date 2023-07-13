c366fe4cac0390b3e0cf303b4d372589fea935a8
Cougheem8 <seryan.1115@gmail.com>
2023-07-13 11:56:30 -0600

    refactored application into modules

--- constants.py
+++
@@ -1,1 +0,0 @@
-BASE_API = 'https://www.amiiboapi.com/api/'
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from view.shell import run_shell
+
+def main() -> None:
+    run_shell()
+
+    
+if __name__ == "__main__":
+    main()
+    
